cfa1e4a4908e89125f5a1683e4b5b03ae1d4e4fe
Isuru Fernando <isuruf@gmail.com>
2021-04-18 12:26:50 -0500

    use cmake directly

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,10 +2,17 @@
 
 # for cross compiling with openmpi
 export OPAL_PREFIX=$PREFIX
+export CC=mpicc
+export CXX=mpicxx
 
-make config \
-     shared=1 \
-     prefix=$PREFIX
+mkdir -p build
+cd build
+cmake ${CMAKE_ARGS} \
+  -DGKLIB_PATH=$SRC_DIR/metis/GKlib \
+  -DMETIS_PATH=$SRC_DIR/metis \
+  -DSHARED=1 \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  ..
 
-make
+make -j${CPU_COUNT}
 make install
